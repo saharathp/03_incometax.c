a5be81c0fa0e2c310fda6ed49ff96c0b395423e4
saharathp <126144646+saharathp@users.noreply.github.com>
2023-06-28 22:01:43 +0545

    Add files via upload

---
+++ 03_incometax.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    float tax=0,income;
+    printf("Enter your income:");
+    scanf("%f",&income);
+    if(income>=250000 && income<=500000){
+        tax=tax+ 0.05*(income-250000);
+    }
+    if(income>=500000 && income<=1000000){
+        tax=tax+ 0.10*(income-500000);
+    }
+    if(income>=1000000){
+        tax=tax+ 0.30*(income-1000000);
+    }
+    printf("Your net income tax to be paid by 26th of the month=%f\n",tax);
+    return 0;
+}
